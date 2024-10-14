c1dce5980663445609629930cd915c6b6eb42545
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-10-14 23:07:44 +0300

    add with-custom-auth-check

--- AuthWith2Fa/Controllers/ValuesController.cs
+++ AuthWith2Fa/Controllers/ValuesController.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthWith2Fa.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        [Authorize]
         [HttpGet]
         public IActionResult Get()
         {
+            return Ok("asdf");
+        }
+
+        [HttpGet("with-custom-auth-check")]
+        public IActionResult GetWithCheck()
+        {
+            if (User.Identity != null && !User.Identity.IsAuthenticated)
+            {
+                // Возвращаем кастомный ответ с данными
+                return Unauthorized(new { error = "не авторизован", status = false, data = "дополнительные данные" });
+            }
             return Ok("asdf");
         }
     }
